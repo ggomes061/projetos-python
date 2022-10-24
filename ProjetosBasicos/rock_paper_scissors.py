2f88e65784781ab89638f3cad6b8db8bfda77840
Gustavo Gomes <111575613+ggomes061@users.noreply.github.com>
2022-10-24 18:03:23 -0300

    Add files via upload

---
+++ ProjetosBasicos/rock_paper_scissors.py
@@ -0,0 +1,39 @@
+import random
+
+user_wins = 0
+computer_wins = 0
+
+options = ["rock", "paper", "scissors"]
+
+while True:
+    user_input = input('Type rock/paper/scissors or Q to quit: ')
+    if user_input == 'q':
+        break
+
+    if user_input not in options:
+        continue
+
+    random_number = random.randint(0, 2)
+    computer_pick = options[random_number]
+    print('Computer picked', computer_pick + '.')
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print('You won!')
+        user_wins += 1
+        continue
+    elif user_input == "paper" and computer_pick == "rock":
+        print('You won!')
+        user_wins += 1
+        continue
+    elif user_input == "scissors" and computer_pick == "paper":
+        print('You won!')
+        user_wins += 1
+        continue
+
+    else:
+        print('You lost!!')
+        computer_wins += 1
+
+print(f"You won", {user_wins}, 'times.')
+print(f'The computer won', {computer_wins}, 'times.')
+print('Goodbye!!')
