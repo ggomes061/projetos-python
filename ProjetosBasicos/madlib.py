ef91883fbeb0e54f2863b149939e74a1b2fe80e0
Guztavz <111575613+ggomes061@users.noreply.github.com>
2022-10-24 13:57:39 -0300

    Update madlib.py

--- ProjetosBasicos/madlib.py
+++ ProjetosBasicos/madlib.py
@@ -1,7 +1,7 @@
-adj = input("Adjetives: ")
-verb1 = input("Verb: ")
-verb2 = input("Verb: ")
-famous_person = input("Famous person: ")
+adj = input("Adjetivo: ")
+verb1 = input("Verbo: ")
+verb2 = input("Verbo: ")
+famous_person = input("Uma pessoa Famosa: ")
 
 madlib = f"A programação de computadores é tão {adj}! Isso me deixa tão excitado o tempo todo porque eu amo {verb1}. Mantenha-se hidratado e  {verb2} como se fosse o {famous_person}!"
 
