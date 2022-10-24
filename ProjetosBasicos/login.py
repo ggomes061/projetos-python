a36a32c521a1b90543e46a5a25d63e0618250c45
Guztavz <111575613+ggomes061@users.noreply.github.com>
2022-10-24 13:56:28 -0300

    Add files via upload

---
+++ ProjetosBasicos/login.py
@@ -0,0 +1,30 @@
+from asyncore import loop
+import re
+import time
+
+#pagina de login em python basica!
+
+username = "admin"
+password = "admin"
+
+username_input = input("Username: ")
+password_input = input("Password: ")
+
+if username_input == username and password_input == password:
+    print('Analisando...')
+    print('Aguarde...')
+    time.sleep(5)
+    print('loading...')
+    time.sleep(1)
+    print('...')
+    time.sleep(1)
+    print('...')
+    print('Login Aprovado! redirecionando para pagina inicial.')
+
+else:
+    print('Loading...')
+    time.sleep(3)
+    print('...')
+    time.sleep(1)
+    print('Username ou senha incorreto!')
+    print('Tente novamente!')
---
+++ ProjetosBasicos/madlib.py
@@ -0,0 +1,8 @@
+adj = input("Adjetives: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"A programação de computadores é tão {adj}! Isso me deixa tão excitado o tempo todo porque eu amo {verb1}. Mantenha-se hidratado e  {verb2} como se fosse o {famous_person}!"
+
+print(madlib)
